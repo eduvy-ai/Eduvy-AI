04468ae7443d13cfca2825fdd072207abafa0b0e
pradip pawar <pradip.pawar@thegatewaycorp.co.in>
2026-07-28 18:37:26 +0530

    chore: update Capacitor dependencies to version 8.4.2

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pradip.eai',
+  appName: 'eduvy-ai',
+  webDir: 'dist'
+};
+
+export default config;
